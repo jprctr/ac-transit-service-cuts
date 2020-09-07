6137231a8dd99a792f61492a44d16cb5d43d4a87
jprctr <mail@jamesaproctor.com>
2020-09-07 00:46:29 -0700

    Updates

---
+++ precache-manifest.c54809e8ea3a30d034a18d1aae5d5c3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "684be650c4cd81e9026197a27cb2ea87",
+    "url": "/ac-transit-service-cuts/index.html"
+  },
+  {
+    "revision": "5fc41485acb5202659bb",
+    "url": "/ac-transit-service-cuts/static/css/main.55209566.chunk.css"
+  },
+  {
+    "revision": "5f76d0469d1918944045",
+    "url": "/ac-transit-service-cuts/static/js/2.3801a514.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ac-transit-service-cuts/static/js/2.3801a514.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fc41485acb5202659bb",
+    "url": "/ac-transit-service-cuts/static/js/main.d0606ccf.chunk.js"
+  },
+  {
+    "revision": "ebf257989383b8c0c62d",
+    "url": "/ac-transit-service-cuts/static/js/runtime-main.5641b33d.js"
+  }
+]);
